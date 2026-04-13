08a4ea740479cc6aea6efbfe85045d73fad04d7b
Hulisani <ST10479672@rcconnect.edu.za>
2026-04-13 21:15:02 +0200

    GreetingVoice class including audio player logic

--- greetingVoice.cs
+++ greetingVoice.cs
@@ -6,14 +6,15 @@ namespace POE
 {
     public class greetingVoice
     {
+
         public greetingVoice()
         {
-            
+
             string path_directory = AppDomain.CurrentDomain.BaseDirectory;
-                      
+
             string directory = path_directory.ToLower().Replace("bin\\debug\\net8.0", "");
 
-            
+
             string voice_path = Path.Combine(directory, "AI POE voice.wav");
 
             play_voice(voice_path);
@@ -23,7 +24,7 @@ namespace POE
         {
             try
             {
-              
+
                 using (SoundPlayer player = new SoundPlayer(filePath))
                 {
                     player.Load();
